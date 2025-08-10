c13427971151c7fa68eafcd5ee4201e1eba1bcae
kankodu <reflectivecamouflage@gmail.com>
2025-08-10 14:19:34 +0530

    feat: initial commit for periphery contracts

---
+++ test/AssetToAssetSwapHook.t.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.13;
+
+import {YieldHarvestingHookTest} from "test/YieldHarvestingHook.t.sol";
+import {AssetToAssetSwapHook} from "src/Helpers/AssetToAssetSwapHook.sol";
+import {HookMiner} from "lib/v4-periphery/src/utils/HookMiner.sol";
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {Constants} from "@uniswap/v4-core/test/utils/Constants.sol";
+import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
+import {IPoolManager, SwapParams} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {PoolSwapTest} from "@uniswap/v4-core/src/test/PoolSwapTest.sol";
+import {TickMath} from "@uniswap/v4-core/src/libraries/TickMath.sol";
+import {ModifyLiquidityParams, SwapParams} from "lib/v4-periphery/lib/v4-core/src/types/PoolOperation.sol";
+import {StateLibrary} from "@uniswap/v4-core/src/libraries/StateLibrary.sol";
+import {PoolManager} from "@uniswap/v4-core/src/PoolManager.sol";
+import {BalanceDelta} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
+import {SafeCast} from "lib/openzeppelin-contracts/contracts/utils/math/SafeCast.sol";
+
+contract AssetToAssetSwapHookTest is YieldHarvestingHookTest {
+    AssetToAssetSwapHook assetToAssetSwapHook;
+
+    using StateLibrary for PoolManager;
+
+    uint160 constant SWAP_HOOK_PERMISSIONS =
+        uint160(Hooks.BEFORE_SWAP_FLAG) | uint160(Hooks.BEFORE_SWAP_RETURNS_DELTA_FLAG);
+
+    PoolKey assetsPoolKey;
+
+    function setUp() public override {
+        super.setUp();
+
+        setUpVaults(false);
+
+        (uint160 sqrtRatioX96,,,) = poolManager.getSlot0(poolKey.toId());
+
+        ModifyLiquidityParams memory liquidityParams = ModifyLiquidityParams({
+            tickLower: TickMath.minUsableTick(poolKey.tickSpacing),
+            tickUpper: TickMath.maxUsableTick(poolKey.tickSpacing),
+            liquidityDelta: 1e18,
+            salt: keccak256(abi.encodePacked(address(this), SWAP_HOOK_PERMISSIONS))
+        });
+
+        modifyLiquidity(liquidityParams, sqrtRatioX96);
+
+        (, bytes32 salt) = HookMiner.find(
+            address(this),
+            SWAP_HOOK_PERMISSIONS,
+            type(AssetToAssetSwapHook).creationCode,
+            abi.encode(poolManager, vaultWrapper0, vaultWrapper1, yieldHarvestingHook)
+        );
+
+        assetToAssetSwapHook =
+            new AssetToAssetSwapHook{salt: salt}(poolManager, vaultWrapper0, vaultWrapper1, yieldHarvestingHook);
+
+        assetsPoolKey = PoolKey({
+            currency0: Currency.wrap(address(asset0)),
+            currency1: Currency.wrap(address(asset1)),
+            fee: poolKey.fee,
+            tickSpacing: poolKey.tickSpacing,
+            hooks: assetToAssetSwapHook
+        });
+
+        poolManager.initialize(assetsPoolKey, Constants.SQRT_PRICE_121_100);
+    }
+
+    function test_assetsSwapExactAmountIn() public {
+        uint256 amountIn = 1e18;
+
+        asset0.mint(address(this), amountIn);
+        asset0.approve(address(swapRouter), amountIn);
+
+        //we assume that prior to the mint, poolManager already has some asset0 that user can take it out of
+        asset0.mint(address(poolManager), amountIn);
+
+        uint256 asset1BalanceBefore = asset1.balanceOf(address(this));
+
+        SwapParams memory swapParams = SwapParams({
+            zeroForOne: true,
+            amountSpecified: -int256(amountIn),
+            sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
+        });
+
+        BalanceDelta swapDelta = swapRouter.swap(
+            assetsPoolKey, swapParams, PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false}), ""
+        );
+
+        uint256 asset1Out = SafeCast.toUint256(swapDelta.amount1());
+        assertEq(asset1Out, asset1.balanceOf(address(this)) - asset1BalanceBefore, "Incorrect asset1 out amount");
+    }
+
+    function test_assetsSwapExactAmountOut() public {
+        uint256 amountOut = 1e18;
+
+        asset0.mint(address(this), 18453516209681194614061326345088274511);
+        asset0.approve(address(swapRouter), 18453516209681194614061326345088274511);
+
+        //we assume that prior to the mint, poolManager already has some asset0 that user can take it out of
+        asset0.mint(address(poolManager), 18453516209681194614061326345088274511);
+
+        uint256 asset1BalanceBefore = asset1.balanceOf(address(this));
+
+        SwapParams memory swapParams = SwapParams({
+            zeroForOne: true,
+            amountSpecified: int256(amountOut),
+            sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
+        });
+
+        BalanceDelta swapDelta = swapRouter.swap(
+            assetsPoolKey, swapParams, PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false}), ""
+        );
+
+        assertEq(amountOut, asset1.balanceOf(address(this)) - asset1BalanceBefore, "Incorrect asset1 out amount");
+    }
+}
