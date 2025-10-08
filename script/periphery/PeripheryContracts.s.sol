d738781ca9be1cbd3e53868d9946e8debb15293b
kankodu <reflectivecamouflage@gmail.com>
2025-10-08 16:30:42 +0800

    fix: add additional helper deployment script example

--- script/periphery/PeripheryContracts.s.sol
+++ script/periphery/PeripheryContracts.s.sol
@@ -2,13 +2,17 @@
 pragma solidity ^0.8.26;
 
 import {Script, console} from "forge-std/Script.sol";
-import {AssetToAssetSwapHookForERC4626} from "src/periphery/AssetToAssetSwapHookForERC4626.sol";
+import {AssetToAssetSwapHookForERC4626, IERC4626} from "src/periphery/AssetToAssetSwapHookForERC4626.sol";
 import {LiquidityHelper} from "src/periphery/LiquidityHelper.sol";
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
 import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
 import {HookMiner} from "lib/v4-periphery/src/utils/HookMiner.sol";
 import {YieldHarvestingHook} from "src/YieldHarvestingHook.sol";
 import {IPositionManager} from "lib/v4-periphery/src/interfaces/IPositionManager.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
+import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
+import {TickMath} from "@uniswap/v4-core/src/libraries/TickMath.sol";
 
 contract PeripheryContractsScript is Script {
     uint160 constant HOOK_PERMISSIONS = uint160(Hooks.BEFORE_SWAP_FLAG) | uint160(Hooks.BEFORE_SWAP_RETURNS_DELTA_FLAG)
@@ -36,8 +40,26 @@ contract PeripheryContractsScript is Script {
         AssetToAssetSwapHookForERC4626 assetToAssetSwapHook =
             new AssetToAssetSwapHookForERC4626{salt: assetToAssetSalt}(poolManager, yieldHarvestingHook, owner);
 
+        // AssetToAssetSwapHookForERC4626 assetToAssetSwapHook =
+        //     AssetToAssetSwapHookForERC4626(0x604E6C45FEe7D7634865603c37Ef1695D0f2C888);
+
         // Deploy LiquidityHelper
         LiquidityHelper liquidityHelper = new LiquidityHelper(evc, positionManager, yieldHarvestingHook);
+
+        PoolKey memory poolKey = PoolKey({
+            currency0: Currency.wrap(0x078D782b760474a361dDA0AF3839290b0EF57AD6), // USDC
+            currency1: Currency.wrap(0x9151434b16b9763660705744891fA906F660EcC5), // USDT0
+            fee: 18,
+            tickSpacing: 1,
+            hooks: IHooks(address(assetToAssetSwapHook))
+        });
+
+        poolManager.initialize(poolKey, TickMath.getSqrtPriceAtTick(0));
+
+        IERC4626 vaultWrapper0 = IERC4626(0x9C383Fa23Dd981b361F0495Ba53dDeB91c750064); // VII-eUSDC
+        IERC4626 vaultWrapper1 = IERC4626(0x7b793B1388e14F03e19dc562470e7D25B2Ae9b97); // VII-eUSDT0
+
+        assetToAssetSwapHook.setDefaultVaultWrappers(poolKey, vaultWrapper0, vaultWrapper1);
 
         vm.stopBroadcast();
     }
